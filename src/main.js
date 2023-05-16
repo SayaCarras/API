94e5b50247cfe19da579977ec1290dc5d2c66f0f
Your Name <you@example.com>
2023-05-16 23:41:49 +0000

    first commit

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
+import compression from 'compression'
+
+const app = express()
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+app.use(compression())
+app.use(helmet())
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000')
+
+})
+
+const router = express.Router()
+
+router.get('/', (req, resp) => {
+
+    console.log('This is / Route')
+    resp.send('This is / Route')
+
+})
+
+app.use(router)
